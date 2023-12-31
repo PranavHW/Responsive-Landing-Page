118a51342b4360110165b0fdb929a24f146e3abb
PranavHW <154511013+PranavHW@users.noreply.github.com>
2023-12-31 10:23:31 +0530

    Add files via upload

---
+++ Project 2/AB.js
@@ -0,0 +1,6 @@
+function redirectToOrderPage() {
+    // You can implement the redirection logic here
+    alert("Redirecting to the order page!");
+    // window.location.href = "order.html"; // Uncomment and replace with your actual order page
+  }
+  
